62687e376f8c9b7abb2ba233d5dac560aefc98c3
anisia1204 <anisia.rosu@student.upt.ro>
2023-04-06 16:29:12 +0300

    refactored EmployeeRestController to make use of EmployeeService instead of using the EmployeeDAO directly

--- src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -1,7 +1,8 @@
 package com.luv2code.cruddemo.rest;
 
-import com.luv2code.cruddemo.dao.EmployeeDAO;
 import com.luv2code.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api") //the base mapping
 public class EmployeeRestController {
-    //inject employee dao directly for now using constructor injection
-    private EmployeeDAO employeeDAO;
-    public EmployeeRestController(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    private EmployeeService employeeService;
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
     //expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll(); //simply delegate the work to the DAO
+        return employeeService.findAll(); //simply delegate the work to the DAO
     }
 }
