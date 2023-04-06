7c0cf77370b73db10d8ceb2d8f61ba86149d211c
anisia1204 <anisia.rosu@student.upt.ro>
2023-04-06 17:33:20 +0300

    added GET /employees/{employeeID} endpoint

--- src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.cruddemo.entity.Employee;
 import com.luv2code.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,15 @@ public class EmployeeRestController {
     //expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeService.findAll(); //simply delegate the work to the DAO
+        return employeeService.findAll(); //simply delegate the work to the employee service
+    }
+
+    //add mapping for GET /employees/{employeeID}
+    @GetMapping("/employees/{employeeID}")
+    public Employee getEmployee(@PathVariable  int employeeID) {
+        Employee employee = employeeService.findById(employeeID); //simply delegate the work to the employee service
+        if(employee == null)
+            throw new RuntimeException("Employee id not found " + employeeID);
+        return employee;
     }
 }
